b603c469d12a806debb3eea086112a6e08b22068
Schmidt, Jeff <jbschmi3@ilstu.edu>
2018-11-14 19:05:21 -0600

    small UI update

--- src/main/java/BackDatUp.java
+++ src/main/java/BackDatUp.java
@@ -6,21 +6,27 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.JTextPane;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
+import javax.swing.SpringLayout;
 
 public class BackDatUp {
 
-	private JFrame frame;
+	private JFrame BackDatUp;
 
 	/**
 	 * Launch the application.
@@ -30,13 +36,13 @@ public class BackDatUp {
 			public void run() {
 				try {
 					BackDatUp window = new BackDatUp();
-					window.frame.setVisible(true);
-					window.frame.setPreferredSize(new Dimension(800, 600));
-					window.frame.pack();
-					window.frame.setResizable(false);
-					window.frame.setLocationRelativeTo(null);
-					window.frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-					window.frame.addWindowListener(new WindowAdapter() {
+					window.BackDatUp.setVisible(true);
+					window.BackDatUp.setPreferredSize(new Dimension(800, 600));
+					window.BackDatUp.pack();
+					window.BackDatUp.setResizable(false);
+					window.BackDatUp.setLocationRelativeTo(null);
+					window.BackDatUp.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+					window.BackDatUp.addWindowListener(new WindowAdapter() {
 						@Override
 						public void windowClosing(WindowEvent we) {
 							String ObjButtons[] = {"Yes", "No"};
@@ -65,72 +71,78 @@ public class BackDatUp {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		gridBagLayout.columnWidths = new int[]{0, 0};
-		gridBagLayout.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gridBagLayout.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gridBagLayout.rowWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		frame.getContentPane().setLayout(gridBagLayout);
+		
+		//ArrayList of jobQueue
+		ArrayList<String> jobQueue = new ArrayList<>();
+		//test variables
+		jobQueue.add("Job 1: Scheduled for 12/12/18 14:30\n");
+		jobQueue.add("Job 2: Scheduled for 12/13/18 10:22\n");
+		
+		BackDatUp = new JFrame();
+		BackDatUp.setBounds(100, 100, 450, 300);
+		BackDatUp.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		SpringLayout springLayout = new SpringLayout();
+		BackDatUp.getContentPane().setLayout(springLayout);
 		
 		JLabel lblJobQueue = new JLabel("Job Queue:");
-		GridBagConstraints gbc_lblJobQueue = new GridBagConstraints();
-		gbc_lblJobQueue.anchor = GridBagConstraints.WEST;
-		gbc_lblJobQueue.insets = new Insets(0, 0, 5, 0);
-		gbc_lblJobQueue.gridx = 0;
-		gbc_lblJobQueue.gridy = 0;
-		frame.getContentPane().add(lblJobQueue, gbc_lblJobQueue);
+		springLayout.putConstraint(SpringLayout.NORTH, lblJobQueue, 0, SpringLayout.NORTH, BackDatUp.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, lblJobQueue, 0, SpringLayout.WEST, BackDatUp.getContentPane());
+		BackDatUp.getContentPane().add(lblJobQueue);
 		
 		JTextPane textPane = new JTextPane();
+		springLayout.putConstraint(SpringLayout.NORTH, textPane, 19, SpringLayout.NORTH, BackDatUp.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, textPane, 0, SpringLayout.WEST, BackDatUp.getContentPane());
+		springLayout.putConstraint(SpringLayout.SOUTH, textPane, 209, SpringLayout.NORTH, BackDatUp.getContentPane());
 		textPane.setEditable(false);
-		GridBagConstraints gbc_textPane = new GridBagConstraints();
-		gbc_textPane.gridheight = 5;
-		gbc_textPane.insets = new Insets(0, 0, 5, 0);
-		gbc_textPane.fill = GridBagConstraints.BOTH;
-		gbc_textPane.gridx = 0;
-		gbc_textPane.gridy = 1;
-		frame.getContentPane().add(textPane, gbc_textPane);
+		BackDatUp.getContentPane().add(textPane);
+		
+		//setup for adding to textPane
+		Document doc = textPane.getDocument();
+		//add each element of jobQueue ArrayList to the pane
+		for(String s:jobQueue) {
+			try {
+				doc.insertString(doc.getLength(), s, null);
+			} catch (BadLocationException e) {
+				e.printStackTrace();
+			}
+		}
 		
 		JButton btnScheduleJob = new JButton("Schedule a job");
+		springLayout.putConstraint(SpringLayout.WEST, btnScheduleJob, -187, SpringLayout.EAST, BackDatUp.getContentPane());
 		btnScheduleJob.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		
 		JButton btnDisplayBackups = new JButton("Display Backups");
-		GridBagConstraints gbc_btnDisplayBackups = new GridBagConstraints();
-		gbc_btnDisplayBackups.insets = new Insets(0, 0, 5, 0);
-		gbc_btnDisplayBackups.gridx = 0;
-		gbc_btnDisplayBackups.gridy = 6;
-		frame.getContentPane().add(btnDisplayBackups, gbc_btnDisplayBackups);
-		GridBagConstraints gbc_btnScheduleJob = new GridBagConstraints();
-		gbc_btnScheduleJob.insets = new Insets(0, 0, 5, 0);
-		gbc_btnScheduleJob.gridx = 0;
-		gbc_btnScheduleJob.gridy = 7;
-		frame.getContentPane().add(btnScheduleJob, gbc_btnScheduleJob);
+		springLayout.putConstraint(SpringLayout.NORTH, btnScheduleJob, 6, SpringLayout.SOUTH, btnDisplayBackups);
+		springLayout.putConstraint(SpringLayout.EAST, btnScheduleJob, 0, SpringLayout.EAST, btnDisplayBackups);
+		springLayout.putConstraint(SpringLayout.EAST, textPane, -35, SpringLayout.WEST, btnDisplayBackups);
+		springLayout.putConstraint(SpringLayout.EAST, btnDisplayBackups, -39, SpringLayout.EAST, BackDatUp.getContentPane());
+		springLayout.putConstraint(SpringLayout.WEST, btnDisplayBackups, -187, SpringLayout.EAST, BackDatUp.getContentPane());
+		springLayout.putConstraint(SpringLayout.NORTH, btnDisplayBackups, 19, SpringLayout.NORTH, BackDatUp.getContentPane());
+		BackDatUp.getContentPane().add(btnDisplayBackups);
+		BackDatUp.getContentPane().add(btnScheduleJob);
 		
 		JButton btnDeleteJob = new JButton("Delete a job");
+		springLayout.putConstraint(SpringLayout.NORTH, btnDeleteJob, 6, SpringLayout.SOUTH, btnScheduleJob);
+		springLayout.putConstraint(SpringLayout.WEST, btnDeleteJob, -187, SpringLayout.EAST, BackDatUp.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnDeleteJob, 0, SpringLayout.EAST, btnDisplayBackups);
 		btnDeleteJob.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		GridBagConstraints gbc_btnDeleteJob = new GridBagConstraints();
-		gbc_btnDeleteJob.insets = new Insets(0, 0, 5, 0);
-		gbc_btnDeleteJob.gridx = 0;
-		gbc_btnDeleteJob.gridy = 8;
-		frame.getContentPane().add(btnDeleteJob, gbc_btnDeleteJob);
+		BackDatUp.getContentPane().add(btnDeleteJob);
 		
 		JButton btnEditJob = new JButton("Edit a job");
+		springLayout.putConstraint(SpringLayout.NORTH, btnEditJob, 6, SpringLayout.SOUTH, btnDeleteJob);
+		springLayout.putConstraint(SpringLayout.WEST, btnEditJob, -187, SpringLayout.EAST, BackDatUp.getContentPane());
+		springLayout.putConstraint(SpringLayout.EAST, btnEditJob, 0, SpringLayout.EAST, btnDisplayBackups);
 		btnEditJob.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		GridBagConstraints gbc_btnEditJob = new GridBagConstraints();
-		gbc_btnEditJob.gridx = 0;
-		gbc_btnEditJob.gridy = 9;
-		frame.getContentPane().add(btnEditJob, gbc_btnEditJob);
+		BackDatUp.getContentPane().add(btnEditJob);
 	}
 
 }
