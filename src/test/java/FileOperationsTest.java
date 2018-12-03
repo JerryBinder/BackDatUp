67ce65424a0bb9dc1ddc378a6575270e50063572
Jarred Binder <jarredbinder@gmail.com>
2018-12-03 17:46:00 -0600

    More test fixes

--- src/test/java/FileOperationsTest.java
+++ src/test/java/FileOperationsTest.java
@@ -1,32 +1,35 @@
-import static org.junit.jupiter.api.Assertions.*;
+package test.java;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import main.java.FileOperations;
 
-import junit.framework.Assert;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
 
 class FileOperationsTest {
 
 	FileOperations operations;
-	@BeforeEach
+	@Before
 	void setUp() throws Exception {
 		operations = new FileOperations();
 	}
 
 	@Test
 	void testCreateFolder() {
-		//fail("Not yet implemented");
-		Assert.assertEquals(true, this.operations.createFolder("C:\\Users\\Dustin\\Desktop\\hellothere"));
+		//TODO : create test file in project folder
+		assertEquals(true, this.operations.createFolder("C:\\Users\\Dustin\\Desktop\\hellothere"));
 	}
 	
 	@Test
 	void testCompareHashes() {
-		Assert.assertEquals(false, this.operations.compareHashes("036353ede97e766a573c73a6aae221fb", "3e1ffeb20ef5aa22c9aa1c0eca143acb"));
+		assertEquals(false, this.operations.compareHashes("036353ede97e766a573c73a6aae221fb", "3e1ffeb20ef5aa22c9aa1c0eca143acb"));
 	}
 	
 	@Test
 	void testdeleteFile() {
-		Assert.assertEquals(true, this.operations.deleteFile("C:\\Users\\Dustin\\Desktop\\hellothere"));
+		//TODO : create test file in project folder
+		assertEquals(true, this.operations.deleteFile("C:\\Users\\Dustin\\Desktop\\hellothere"));
 	}
 
 }
--- src/test/java/HashFileUtilTest.java
+++ src/test/java/HashFileUtilTest.java
@@ -1,28 +1,33 @@
-import static org.junit.jupiter.api.Assertions.*;
+package test.java;
 
 import java.io.File;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
 
-import junit.framework.Assert;
+import main.java.HashFileUtil;
 
 class HashFileUtilTest {
+	
 	HashFileUtil hasher;
-	@BeforeEach
+	
+	@Before
 	void setUp() throws Exception {
 		hasher = new HashFileUtil();
 	}
 
 	@Test
 	void generateMd5Hashtest() {
-		Assert.assertEquals(this.hasher.generateMd5Hash("C:\\Users\\Dustin\\Desktop\\hellothere"), this.hasher.generateMd5Hash("C:\\Users\\Dustin\\Desktop\\hellothere"));
+		//TODO : create test file in project folder
+		assertEquals(this.hasher.generateMd5Hash("C:\\Users\\Dustin\\Desktop\\hellothere"), this.hasher.generateMd5Hash("C:\\Users\\Dustin\\Desktop\\hellothere"));
 	}
 	
 	@Test
 	void generateMd5HashTestFile() {
+		//TODO : create test file in project folder
 		File file = new File("C:\\Users\\Dustin\\Desktop\\hellothere");
-		Assert.assertEquals(this.hasher.generateMd5Hash(file), this.hasher.generateMd5Hash(file));
+		assertEquals(this.hasher.generateMd5Hash(file), this.hasher.generateMd5Hash(file));
 	}
 
 }
